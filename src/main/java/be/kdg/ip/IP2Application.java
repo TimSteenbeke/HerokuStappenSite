00a45488168c2e476d3535278b18016e63e00664
TimSteenbeke <tim.steenbeke@student.kdg.be>
2018-02-01 16:21:02 +0100

    test 1e commit

---
+++ src/main/java/be/kdg/ip/IP2Application.java
@@ -0,0 +1,12 @@
+package be.kdg.ip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IP2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IP2Application.class, args);
+    }
+}
---
+++ src/main/java/be/kdg/musicmaker/MusicmakerApplication.java
@@ -0,0 +1,12 @@
+package be.kdg.musicmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicmakerApplication.class, args);
+	}
+}
